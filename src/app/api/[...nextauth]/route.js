55a86e5c5ffb3fd5a67283057d6fba4b3cefc31c
Abraham Duno <abraham.d@servicepad.com>
2025-01-30 19:03:45 -0400

    next auth reorg 3

--- src/app/api/[...nextauth]/route.js
+++ src/app/api/[...nextauth]/route.js
@@ -1,19 +1,15 @@
-import NextAuth from "next-auth"
-// import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-        // GithubProvider({
-        //     clientId: process.env.GITHUB_ID,
-        //     clientSecret: process.env.GITHUB_SECRET,
-        // }),
-    ],
-}
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
