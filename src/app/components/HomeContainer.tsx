133d3c17e1badd66faaa4451a807db35a3656024
Abraham Duno <abraham.d@servicepad.com>
2025-01-30 18:50:46 -0400

    init

---
+++ src/app/components/HomeContainer.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useState } from 'react';
+import HomeContent from './HomeContent'
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function HomeContainer() {
+  // You can add container logic here, such as:
+  // - Data fetching
+  // - State management
+  // - Layout control
+  // - Error boundaries
+  const { data: session, status } = useSession();
+  const handleLogin = () => signIn();
+  const handleLogout = () => signOut();
+  const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <div className="home-container">
+      <HomeContent
+       handleLogin={handleLogin}
+       handleLogout={handleLogout}
+       session={session}
+       searchQuery={searchQuery}
+       setSearchQuery={setSearchQuery}
+      />
+    </div>
+  )
+} 
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MainAppWrapper from './components/MainAppWrapper'
+import HomeContainer from './components/HomeContainer'
+
+export default function Home() {
+  return (
+    <MainAppWrapper>
+      <HomeContainer />
+    </MainAppWrapper>
+  )
+}
